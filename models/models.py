3a463f6fdfddcb2eb2e423e3639de7f92aa549bf
SakshyamSingB <sakshyambhandari1@gmail.com>
2025-06-10 15:25:09 +0545

    Views added for student,teacher and grade

--- models/models.py
+++ models/models.py
@@ -1,19 +1,26 @@
-# -*- coding: utf-8 -*-
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+class ResPartnerInherit(models.Model):
+    _inherit = 'res.partner'
+
+    is_student = fields.Boolean()
+    is_teacher = fields.Boolean()
+    grade_id = fields.Many2many('grade')
+
+    @api.onchange("grade_id")
+    def _onchange_grade(self):
+        self.grade_id = self.grade_id.name
+
+class Subject(models.Model):
+    _name = 'subject'
+
+    name = fields.Char(string="Subject Name")
+    teacher_id = fields.Many2many('res.partner', string="Teacher", domain=[('is_teacher', '=', True)])
+    grade_id = fields.Many2one('grade', string="Grade")
 
 
-# class school_management(models.Model):
-#     _name = 'school_management.school_management'
-#     _description = 'school_management.school_management'
+class Grade(models.Model):
+    _name = 'grade'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
-
+    name = fields.Char(string="Grade Name")
+    student_id = fields.Many2many('res.partner', string="Student", domain=[('is_student', '=', True)])
